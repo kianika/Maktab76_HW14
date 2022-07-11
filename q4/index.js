75e2f17d958b62ff4acd112faccf40284ac47cae
kianika <mona_yaghoubi@gmail.com>
2022-07-11 23:45:39 +0200

    v2

--- q4/index.js
+++ q4/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("button").click(function(){
-        $("p").css("background-color", "yellow");
-        $("p").css("border-style", "dashed");
-        $("p").css("border-color", "blue");
+        $("p , span").css("background-color", "yellow");
+        $("p , span").css("border-style", "dashed");
+        $("p , span").css("border-color", "blue");
     });
   });
