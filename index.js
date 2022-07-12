a17c168f41ee206ddc31517f1e7574df8a593e97
kianika <mona_yaghoubi@gmail.com>
2022-07-12 10:27:41 +0200

    v5

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
             url: API_URL,
             type: "GET",
             success: function(infos) {
+                $("table tbody").append('<tr style="background-color:yellow;"><th>First Name</th><th>Last Name</th><th>Age</th></tr>');
             $.each(infos, function (index , info) {
                 var row = "<tr><td>" + info.name + "</td><td>" + info.lastname + "</td><td>" + info.age + "</td></tr>";
      $("table tbody").append(row);
