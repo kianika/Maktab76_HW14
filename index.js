5d05b55440ff9ae850a3857125c575fd015a57b3
kianika <mona_yaghoubi@gmail.com>
2022-07-12 08:55:15 +0200

    v3

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-/* 
 
-const API_URL = 'https://62cc9b41a080052930adefdc.mockapi.io/info';
-$(document).ready(function(){
-$("#btn").click(function () {
-const table = $('table tbody');
-$.ajax({
-url: API_URL,
-type: "GET",
-success: function(employees) {
-table.append(`idfirstNamelastNamephoneNumberemailaddressjobavator`)
-$.each(employees, function (index , employee) {
-table.append(
-`${employee.id}${employee.firstName}${employee.lastName}${employee.phoneNumber}${employee.email}${employee.address}${employee.job}`
-);
-});
-}
-});
-});
-});
- */
 
 const API_URL = 'https://62cc9b41a080052930adefdc.mockapi.io/info';
 $(document).ready(function(){
